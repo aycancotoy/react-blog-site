7ececcdaf910116235bfa85a85357c09045b5bdd
aycancotoy <aycancotoy@gmail.com>
2022-03-14 17:18:52 +0300

    header tamamlandı.

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -4,9 +4,10 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-          <span>React & Node</span>
-          <span>Blog</span>
+          <span className="headerTitleSm">React & Node</span>
+          <span className="headerTitleLg">Blog</span>
       </div>
+      <img className="headerImg" src="https://i.picsum.photos/id/497/1920/1080.jpg?hmac=lCmyuUeNJbmkWXfeLeQ2SxRu3mII34_GIdUIBn3M6RQ" alt="" />
     </div>
   );
 }
