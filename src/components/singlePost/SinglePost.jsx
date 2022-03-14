1d3e561545c431dd275c0f8437e03787f715ed49
aycancotoy <aycancotoy@gmail.com>
2022-03-15 00:14:36 +0300

    Post sayfası oluşturuldu, css de değişiklikler yapıldı

---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,44 @@
+import "./singlePost.css";
+
+export default function SinglePost() {
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img
+          src="https://i.picsum.photos/id/630/1400/500.jpg?hmac=Xk9LETXpFnBDikWqDmyEOPUNbM681mWHYjeWlexXHhM"
+          alt=""
+          className="singlePostImg"
+        />
+        <h1 className="singlePostTitle">
+          Lorem ipsum dolor sit amet.
+          <div className="singlePostEdit">
+            <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
+            <i className="singlePostIcon fa-solid fa-trash-can"></i>
+          </div>
+        </h1>
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">
+            Author: <b>Aycan</b>
+          </span>
+          <span className="singlePostDate">1 hour ago</span>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi eveniet
+          vel debitis eligendi quidem facere nihil aut laboriosam, amet
+          reiciendis soluta, quis aperiam, assumenda impedit veritatis tempore
+          expedita sint quam? Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Nisi eveniet vel debitis eligendi quidem facere
+          nihil aut laboriosam, amet reiciendis soluta, quis aperiam, assumenda
+          impedit veritatis tempore expedita sint quam? Lorem ipsum dolor sit
+          amet consectetur, adipisicing elit. Nisi eveniet vel debitis eligendi
+          quidem facere nihil aut laboriosam, amet reiciendis soluta, quis
+          aperiam, assumenda impedit veritatis tempore expedita sint quam? Lorem
+          ipsum dolor sit amet consectetur, adipisicing elit. Nisi eveniet vel
+          debitis eligendi quidem facere nihil aut laboriosam, amet reiciendis
+          soluta, quis aperiam, assumenda impedit veritatis tempore expedita
+          sint quam?
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,7 +1,10 @@
-import "./single.css"
+import "./single.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+import SinglePost from "../../components/singlePost/SinglePost";
 
 export default function Single() {
-  return (
-    <div>Single</div>
-  )
+  return <div className="single">
+      <SinglePost/>
+      <Sidebar/>
+  </div>;
 }
