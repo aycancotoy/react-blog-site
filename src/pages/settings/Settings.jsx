6d6e141a8061bd97e40bdcc5b7c74282840befe0
aycancotoy <aycancotoy@gmail.com>
2022-03-15 11:58:47 +0300

    Settings page tamamlandı

---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,36 @@
+import "./settings.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+export default function Settings() {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src="https://i.picsum.photos/id/82/200/200.jpg?hmac=ATNAhTLN2dA0KmTzSE5D9XiPe3GMX8uwxpFlhU7U5OY"
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon fa-solid fa-circle-user"></i>
+            </label>
+            <input type="file" id="fileInput" style={{display:"none"}}/>
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Aycan"/>
+          <label>Email</label>
+          <input type="email" placeholder="aycancotoy@gmail.com"/>
+          <label>Password</label>
+          <input type="password"/>
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+}
